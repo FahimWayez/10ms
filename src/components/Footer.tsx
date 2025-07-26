41a8edf49fa0785525850d78fec72016d8a915c9
Fahim Wayez <fahimwayez1@gmail.com>
2025-07-27 03:15:47 +0600

    footer responsive issue fixed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -33,7 +33,7 @@ export default async function Footer() {
 
   return (
     <footer className="mt-16 border-t border-gray-100 bg-white px-10 lg:px-24 py-2">
-      <div className="container-fluid md:grid grid-cols-1 gap-10 py-10 md:grid-cols-4 flex flex-col md:items-left items-center md:text-left text-center md:justify-normal justify-center">
+      <div className="container-fluid grid grid-cols-1 md:grid-cols-4 gap-10 py-10 text-center md:text-left">
         <div className="space-y-4 justify-center">
           <div className="flex justify-center md:justify-normal">
             <Image
